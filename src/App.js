2576b7c4b2d4dcff86b92b2d24ed92efd65ba922
Tung2709 <vuthanhtungautomation@gmail.com>
2023-07-27 08:56:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BaiTapLayoutComponent from "./BaiTapLayoutComponent/BaiTapLayoutComponent";
+
+const App = () => {
+  return (
+    <div>
+      <BaiTapLayoutComponent />
+    </div>
+  );
+};
+
+export default App;
